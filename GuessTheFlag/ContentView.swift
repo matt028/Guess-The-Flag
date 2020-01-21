66f8850dbf5c2cae762bca2e1bef6b0f8f777751
Matt Sutton <mattsuttondev@gmail.com>
2020-01-21 12:39:27 -0500

    Initial Commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Matthew Sutton on 1/21/20.
+//  Copyright © 2020 Matthew Sutton. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
